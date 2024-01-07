e9956b851a711ac79a60bc70ff30f591428963b3
Shanmugapriya-23 <155863688+Shanmugapriya-23@users.noreply.github.com>
2024-01-06 22:03:03 -0500

    Add files via upload

---
+++ day1/ChromeBrowser.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class ChromeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion=105.2f;
+		String browserName="Chrome";
+		boolean pageLoaded=true;
+		int noOfBrowser=1;
+		char browserLogo='c';
+		System.out.println("Browser Name: "+browserName);
+		System.out.println("Browser Version: "+browserVersion);
+		System.out.println("Is Page Loaded: "+pageLoaded);
+		System.out.println("No of Browser: "+noOfBrowser);
+		System.out.println("Browser Logo:"+browserLogo);		
+		
+	}
+
+}
